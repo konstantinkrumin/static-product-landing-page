7c03e1edd6fae0c484958456e47884d21ff4b44d
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-13 00:22:57 +0300

    Updates

---
+++ precache-manifest.e57609d98502e0621a8e0f7df242fcb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7ad63e4296878a1d52cd0f8ef694da",
+    "url": "/static-product-landing-page/index.html"
+  },
+  {
+    "revision": "87c6ab134e0f32fd9ec1",
+    "url": "/static-product-landing-page/static/css/main.314bdaf0.chunk.css"
+  },
+  {
+    "revision": "60f46b2fb7a884600a9a",
+    "url": "/static-product-landing-page/static/js/2.fc15138c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static-product-landing-page/static/js/2.fc15138c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c6ab134e0f32fd9ec1",
+    "url": "/static-product-landing-page/static/js/main.ff45d6a1.chunk.js"
+  },
+  {
+    "revision": "ccf3a945ab6f82dd6a40",
+    "url": "/static-product-landing-page/static/js/runtime-main.65b1f1eb.js"
+  }
+]);
